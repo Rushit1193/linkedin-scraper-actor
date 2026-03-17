7a544cc098f1f133f4126672e266df3f6c110d23
Rushit Sangani <rushitsangani11@gmail.com>
2026-03-17 11:33:16 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 await Actor.init();
 
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyWJN1kcwtomVHzCtdXSsW3ScEYsea7C3bPRVyWJbCifCr77TNXDZLAVUhodib8rESOwA/exec';
-const WEBHOOK_INPUT_URL = 'https://n8n-main.chitlangia.co/webhook/private-profiles-scraper-request';
-const WEBHOOK_STATUS_URL = 'https://n8n-main.chitlangia.co/webhook/private-profile-export-request-stats';
+const WEBHOOK_INPUT_URL = 'https://s1.boomerangserver.co.in/webhook/all-services-caller';
+const WEBHOOK_STATUS_URL = 'https://s1.boomerangserver.co.in/webhook/private-profile-export-request-stats';
 
 const input = await Actor.getInput();
 console.log('Input received:', input);
